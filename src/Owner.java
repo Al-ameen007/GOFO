75a28f286a08e3d84f8b380d93d7d65949fdb86f
SCJoe-bit <mohammedyoussef111@gmail.com>
2021-06-06 22:21:47 +0200

    modified doing the owner class

--- src/Owner.java
+++ src/Owner.java
@@ -1,6 +1,56 @@
 import java.util.ArrayList;
 
 public class Owner extends Person{
-     ArrayList<Playground> playgrounds = new ArrayList<Playground>();
-
+     private ArrayList<Playground> playgrounds = new ArrayList<Playground>();
+     private OwnerDB request;
+     public int counter =0;
+     public boolean registerPlayground(String name, String location, String size, float price, float totalHR){
+          playgrounds.get(counter).setName(name);
+          playgrounds.get(counter).setLocation(location);
+          playgrounds.get(counter).setSize(size);
+          playgrounds.get(counter).setPrice(price);
+          playgrounds.get(counter).setTotalHR(totalHR);
+          counter++;
+          return true;
+     }
+     public boolean editPlayGround(String choice,String name,Object var ){
+          for(int i=0;i<=counter;i++){
+               if(name == playgrounds.get(i).getName()){
+                    if(choice.equals("name")){
+                         playgrounds.get(i).setName(var);
+                    }else if(choice.equals("location")){
+                         playgrounds.get(i).setLocation(var);
+                    }else if(choice.equals("size")){
+                         playgrounds.get(i).setSize(var);
+                    }else if(choice.equals("price")){
+                         playgrounds.get(i).setPrice(var);
+                    }else if(choice.equals("totalHR")){
+                         playgrounds.get(i).setTotalHR(var);
+                    }
+               }
+          }
+          return true;
+     }
+     public void displayPlayGrounds() {
+          for(int i=0;i<=counter;i++){
+               int num=i+1;
+               System.out.println("Playground number: " + num);
+               System.out.println("Name: " + playgrounds.get(i).getName());
+               System.out.println("Location: " + playgrounds.get(i).getlocation());
+               System.out.println("Size: " +  playgrounds.get(i).getSize());
+               System.out.println("Price: " +  playgrounds.get(i).getPrice());
+               System.out.println("Total hours available per day: " + playgrounds.get(i).getTotalHR());
+               System.out.println("====================================");
+          }
+     }
+     public Owner signUp(String name,String email,String password,String location,int phone){
+          Owner owner = new Owner();
+          owner.setName(name);
+          owner.setEmail(email);
+          owner.setPassword(password);
+          owner.setLocation(location);
+          owner.setPhone(phone);
+          request.saveData(owner);
+          return owner;
+     }
 }
