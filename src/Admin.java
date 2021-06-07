a775e693026fb9f3d9230c316a2029e75e4ba1f4
SCJoe-bit <mohammedyoussef111@gmail.com>
2021-06-07 13:59:41 +0200

    modified the admin class

--- src/Admin.java
+++ src/Admin.java
@@ -1,2 +1,37 @@
 public class Admin extends Person{
+    private Database data;
+    public boolean approvePlayground(Playground playground){
+
+    }
+    public boolean suspend(Playground playground){
+
+    }
+    public boolean reactivate(Playground playground){
+
+    }
+    public boolean delete(Playground playground){
+
+    }
+    public void display(String name) {
+        for (int i = 0; i <= data.playgrounds.getCounter(); i++) {
+            if (name == data.playgrounds.get(i).getName()) {
+                System.out.println("Playground:");
+                System.out.println("Name: " + data.playgrounds.get(i).getName());
+                System.out.println("Location: " + data.playgrounds.get(i).getLocation());
+                System.out.println("Size: " + data.playgrounds.get(i).getSize());
+                System.out.println("Price: " + data.playgrounds.get(i).getPrice());
+                System.out.println("Total hours available per day: " + data.playgrounds.get(i).getTotalAvailableHoursToBook());
+            }
+        }
+    }
+    public Admin signUp(String name,String email,String password,String location,String phone){
+        Admin admin = new Admin();
+        admin.setName(name);
+        admin.setEmail(email);
+        admin.setPassword(password);
+        admin.setLocation(location);
+        admin.setPhone(phone);
+        data.saveData(admin);
+        return admin;
+    }
 }
