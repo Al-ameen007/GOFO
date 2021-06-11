489ef19f1cadc0fe09a205ffd12b8da5b74c3444
SCJoe-bit <mohammedyoussef111@gmail.com>
2021-06-11 16:56:19 +0200

    modified the admin,owner,request class

--- src/Owner.java
+++ src/Owner.java
@@ -12,12 +12,28 @@ public class Owner extends Person {
     private ArrayList<Playground> playgrounds = new ArrayList<Playground>();
     private ArrayList<Request> myRequests = new ArrayList<Request>();
 
-
+    /**
+     * uded to register the owner's playgrounds
+     * @param name
+     * @param location
+     * @param size
+     * @param price
+     * @param cancellationPeriod
+     * @param totalHR
+     * @param start
+     * @param end
+     */
     public void registerPlayground(String name, String location, String size, int price, float cancellationPeriod, int totalHR, int start, int end) {
         Playground myPG = new Playground(name, location, size, price, cancellationPeriod, totalHR, this, start, end);
 
     }
 
+    /**
+     * Used to edit specific playground
+     * @param choice
+     * @param name
+     * @param var
+     */
     public void editPlayground(String choice, String name, String var) {
         int n = playgrounds.size();
         for (int i = 0; i < n; i++) {
@@ -53,7 +69,9 @@ public class Owner extends Person {
         }
     }
 
-
+    /**
+     * get all the playgrounds of an owner
+     */
     public void getMyPlaygrounds() {
         int n = Database.playgrounds.size();
         for (int i = 0; i < n; i++) {
@@ -63,6 +81,9 @@ public class Owner extends Person {
         }
     }
 
+    /**
+     * Used to display all playground of an owner
+     */
     public void displayPlayGrounds() {
         getMyPlaygrounds();
         int n = playgrounds.size();
@@ -71,6 +92,14 @@ public class Owner extends Person {
         }
     }
 
+    /**
+     * Used to Owner register
+     * @param name
+     * @param email
+     * @param password
+     * @param location
+     * @param phone
+     */
     public void signUp(String name, String email, String password, String location, String phone) {
         this.setName(name);
         this.setEmail(email);
@@ -81,14 +110,27 @@ public class Owner extends Person {
 
     }
 
+    /**
+     * used to add the request of a booking
+     * @param req
+     */
     public void addRequest(Request req) {
         myRequests.add(req);
     }
 
+    /**
+     * used to get all the requests
+     * @return
+     */
     public ArrayList<Request> getMyRequests() {
         return myRequests;
     }
 
+    /**
+     * used to return specific request
+     * @param id
+     * @return
+     */
     public Request selectReq(int id) {
         int n = myRequests.size();
         for (int i = 0; i < n; i++) {
@@ -101,10 +143,18 @@ public class Owner extends Person {
         return null;
     }
 
+    /**
+     * accept request
+     * @param r
+     */
     public void acceptRequest(Request r) {
         r.accept();
     }
 
+    /**
+     * decline request
+     * @param r
+     */
     public void declineRequest(Request r) {
         r.decline();
     }
